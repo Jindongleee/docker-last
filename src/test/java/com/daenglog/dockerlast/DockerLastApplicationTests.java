2ebf374565e706e026b6571328b6d1853a8e0560
Jindongleee <jindong1019@gmail.com>
2025-05-21 19:07:26 +0900

    ci/cd test

---
+++ src/test/java/com/daenglog/dockerlast/DockerLastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daenglog.dockerlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLastApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
